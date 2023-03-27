3208c2d258cc5f358a748db4aeb1d4231be17699
gi.ran <domotoweilan@gmail.com>
2023-03-27 18:13:01 +0900

    update as lew

--- SQL-EX/HW-1.sql
+++ SQL-EX/HW-1.sql
@@ -7,3 +7,7 @@ DECLARE @a int , @b varchar(10);
 SET @b:= '諸葛亮';
 SET @a:= (SELECT no FROM generals WHERE name = @b);
 UPDATE generals SET status = '一般' WHERE no = @a;
+
+--declare在sqllite上不被支持
+--在Mysql上，declare用在储存过程里
+--定义变量和处理可以由JAV进行处理
