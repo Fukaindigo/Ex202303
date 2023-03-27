012147044bbae311821edba08598465bce92d2b4
gi.ran <domotoweilan@gmail.com>
2023-03-27 16:34:06 +0900

    乱码修改

--- SQL-EX/HW-3.sql
+++ SQL-EX/HW-3.sql
@@ -1,5 +1,5 @@
 CREATE VIEW Homework3 AS
-SELECT a.monarch AS '勢力', COUNT(b.monarch_no) AS '支配都市個数'
+SELECT a.monarch AS , COUNT(b.monarch_no) AS 
 FROM political a
 LEFT JOIN city b ON a.no = b.monarch_no
 GROUP BY a.monarch
